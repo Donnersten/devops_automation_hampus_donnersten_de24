808d484913ca6eb4700553105f95237b65065526
Donnersten <hampus@donnersten.se>
2025-11-04 13:57:43 +0100

    new code

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from flask import Flask
-
+ 
 app = Flask(__name__)
-
+ 
 @app.route('/')
-def home():
-    num1 = 5 
-    num2 = 10
-    return num1 + num2
-
+def hello_world():
+    return '<h1>Hello World!</h1>'
+ 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+ 
